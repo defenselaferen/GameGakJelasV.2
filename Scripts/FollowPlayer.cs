09aabbf528d187723d65eea65d49d7e293483ab7
thekotekjournal <63573571+thekotekjournal@users.noreply.github.com>
2020-07-31 13:41:01 +0700

    Add files via upload

---
+++ Scripts/FollowPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+	// Make The Variable Global
+	public Transform Player;
+	public Vector3 offset;
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.position = Player.position + offset;	
+	}
+}

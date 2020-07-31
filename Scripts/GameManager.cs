4c8b4160eede8961848c2052977b68a539eb74d4
thekotekjournal <63573571+thekotekjournal@users.noreply.github.com>
2020-07-31 14:13:20 +0700

    Add files via upload

---
+++ Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+	bool gameEnd = false;
+	public float waitDelay = 2f;
+
+	public GameObject completeLevelUI;
+
+	public void CompleteLevel()
+	{
+		completeLevelUI.SetActive(true);
+	}
+
+	public void EndGame()
+	{
+		if(gameEnd == false)
+		{
+			gameEnd = true;
+			Invoke("Restart", waitDelay);
+		}
+	}
+
+	void Restart()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+	}
+}
---
+++ Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	// Make The Rigid Body To The Physic
+	public Rigidbody Rb;
+
+	// Make The Variable
+	public float speed = 3000f;
+	public float Jump = 900f;
+	private bool IsJump = false;
+	
+	void Update ()
+	{
+		if(Input.GetKey("a"))
+		{
+			Rb.AddForce(0, 0, -speed * Time.deltaTime);
+		}
+		if(Input.GetKey("d"))
+		{ 
+			Rb.AddForce(0, 0, speed * Time.deltaTime);
+		}
+		if(Input.GetButtonDown("Jump") && IsJump == false)
+		{
+			Rb.AddForce(new Vector3(0, Jump));
+			IsJump = true;
+		}
+
+		if(Rb.position.y < -12.1)
+		{
+			FindObjectOfType<GameManager>().EndGame();
+		}
+	}
+
+	void OnCollisionEnter (Collision other)
+	{
+		if(other.collider.tag == "Platform")
+		{
+			IsJump = false;
+		}
+	}
+}

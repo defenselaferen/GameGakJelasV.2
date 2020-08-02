df0381691ec5d9e7fe51720c1a162a9d3ac6d2ff
thekotekjournal <63573571+thekotekjournal@users.noreply.github.com>
2020-08-02 08:26:38 +0700

    Update File For 480c8fd
    This File For 480c8fd Alpha Game Update

---
+++ Scripts/LevelOptionScript.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelOptionScript : MonoBehaviour {
+
+	public void Level1()
+	{
+		SceneManager.LoadScene("Level1");
+	}
+	public void Level2()
+	{
+		SceneManager.LoadScene("Level2");
+	}
+	public void Level3()
+	{
+		SceneManager.LoadScene("Level3");
+	}
+	public void Level4()
+	{
+		SceneManager.LoadScene("Level4");
+	}
+	public void Level5()
+	{
+		SceneManager.LoadScene("Level5");
+	}
+	public void Level6()
+	{
+		SceneManager.LoadScene("Level6");
+	}
+	public void Level7()
+	{
+		SceneManager.LoadScene("Level7");
+	}
+	public void Level8()
+	{
+		SceneManager.LoadScene("Level8");
+	}
+	public void Level9()
+	{
+		SceneManager.LoadScene("Level9");
+	}
+	public void Level10()
+	{
+		SceneManager.LoadScene("Level10");
+	}
+	public void Level11()
+	{
+		SceneManager.LoadScene("Level11");
+	}
+	public void Level12()
+	{
+		SceneManager.LoadScene("Level12");
+	}
+	public void Level13()
+	{
+		SceneManager.LoadScene("Level13");
+	}
+	public void Level14()
+	{
+		SceneManager.LoadScene("Level14");
+	}
+	public void Level15()
+	{
+		SceneManager.LoadScene("Level15");
+	}
+	public void Level16()
+	{
+		SceneManager.LoadScene("Level16");
+	}
+	public void Level17()
+	{
+		SceneManager.LoadScene("Level17");
+	}
+	public void Level18()
+	{
+		SceneManager.LoadScene("Level18");
+	}
+	public void Level19()
+	{
+		SceneManager.LoadScene("Level19");
+	}
+	public void Level20()
+	{
+		SceneManager.LoadScene("Level20");
+	}
+}
---
+++ Scripts/LobbyScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LobbyScript : MonoBehaviour {
+
+	public void Play()
+	{
+		SceneManager.LoadScene("Level1");
+	}
+	public void Quit()
+	{
+		Application.Quit();
+	}
+	public void LevelOption()
+	{
+		
+	}
+}
